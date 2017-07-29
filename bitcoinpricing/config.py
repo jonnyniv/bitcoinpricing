09bdee50af16853412d8019d938520a8b2d715e4
Jonathan Niven <jonnyniv@gmail.com>
2017-07-29 14:25:42 +0100

    Add config loading function

--- bitcoinpricing/config.py
+++ bitcoinpricing/config.py
@@ -1,22 +1,35 @@
-import yaml
-import jsonschema
+from typing import List
 
+import yaml
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 
 schema = {
-    "type": "array",
-    "items": {
-        "type": "object",
-        "properties": {
-            "name": {"type": "string"},
-            "url": {"type": "string"},
-            "decode": {"type": "array", "items": {"type": "string"}},
-            "params": {"type": "object"}
+    'type': 'array',
+    'items': {
+        'type': 'object',
+        'properties': {
+            'name': {'type': 'string'},
+            'url': {'type': 'string'},
+            'decode': {'type': 'array', 'items': {'type': 'string'}},
+            'params': {'type': 'object'}
         },
-        "required": ["name", "url", "decode"]
+        'required': ['name', 'url', 'decode']
     }
 }
 
 
+def load_site_config(filename: str, config_schema: dict) -> List[dict]:
+    with open(filename, 'r') as f:
+        config = yaml.load(f)
+    try:
+        validate(config, config_schema)
+    except ValidationError:
+        raise
+    else:
+        return config
+
+
 if __name__ == '__main__':
-    with open("sites.yml", 'r') as f:
-        jsonschema.validate(yaml.load(f), schema)
+    with open('sites.yml', 'r') as f:
+        validate(yaml.load(f), schema)
